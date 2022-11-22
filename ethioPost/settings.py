d744d5f7a00907389d855ab5e78e6b5002ba4442
melaku05 <melakushiferaw05@gmail.com>
2022-11-22 10:35:18 +0300

    Setup postgres database

--- ethioPost/settings.py
+++ ethioPost/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ethioPost.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ethioPost',
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'PORT': '5432',
     }
 }
 
