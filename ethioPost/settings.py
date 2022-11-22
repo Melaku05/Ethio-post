a2f586048275d51f1632ee18e9d8c921263deabc
melaku05 <melakushiferaw05@gmail.com>
2022-11-22 10:53:59 +0300

    remove default cache middleware

--- ethioPost/settings.py
+++ ethioPost/settings.py
@@ -47,7 +47,6 @@ INTERNAL_IPS = [
 MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
